d8b07786d9bf3daa62bc65cd01d650a72c02765b
Yusuke Yagyu <gyugyu@pepabo.com>
2018-09-20 11:51:51 +0900

    subtree にしたので js remove

--- UxBoard/interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TaskStatus;
-(function (TaskStatus) {
-    TaskStatus[TaskStatus["Yet"] = 0] = "Yet";
-    TaskStatus[TaskStatus["Doing"] = 1] = "Doing";
-    TaskStatus[TaskStatus["Done"] = 2] = "Done";
-})(TaskStatus = exports.TaskStatus || (exports.TaskStatus = {}));
